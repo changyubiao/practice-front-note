5ba687aa5a6febc40209c60fb883a18945c66905
frank <frank.chang@shoufuyou.com>
2021-11-28 21:30:40 +0800

    add   114-js-bom-dom  动画，录播图

---
+++ js-bom-dom/114-js-bom-dom/animation.js
@@ -0,0 +1,20 @@
+function animation(element, leftEnd, duration, interval) {
+	var leftStart = element.offsetLeft;
+
+	var stepTmp = (leftEnd - leftStart) / (duration / interval);
+	var step = Math.floor(stepTmp);
+	if (step === 0) {
+		step = stepTmp > 0 ? 1 : -1;
+	}
+	element.timer && clearInterval(element.timer);
+
+	element.timer = setInterval(function() {
+		// console.log(element.timer, step, element.offsetLeft - leftEnd);
+		if (Math.abs(leftEnd - element.offsetLeft) <= Math.abs(step)) {
+			element.style.left = leftEnd + 'px';
+			clearInterval(element.timer);
+		} else {
+			element.style.left = (element.offsetLeft + step) + 'px';
+		}
+	}, interval);
+}
